b341e936cb64845169fec0252078f09c245bfe8f
KimPhi <lekimphi1997@gmail.com>
2022-02-02 21:48:01 +0700

    #01: happy lunar new year 2022 - update code for blockchain.

--- blockchain.js
+++ blockchain.js
@@ -1,12 +1,36 @@
-class Blockchain {
+const crypto = require("crypto"), SHA256 = message => crypto.createHash("sha256").update(message).digest("hex");
+class Block {
+    constructor(timestamp = "", data = []) {
+        this.timestamp = timestamp;
+        this.data = data;
+        this.hash = this.getHash();
+        this.prevHash = "";
+        this.nonce = 0;
+    }
+
+    getHash() {
+        return SHA256(this.prevHash + this.timestamp + JSON.stringify(this.data) + this.nonce);
+    }
+
+    mine(difficulty) {
+        while (!this.hash.startsWith(Array(difficulty + 1).join("0"))) {
+            this.nonce++;
+            this.hash = this.getHash();
+        }
+    }
+}
+
+class BlockChain {
     constructor() {
         this.chain = [new Block(Date.now().toString())];
         this.difficulty = 1;
         this.blockTime = 30000;
     }
-    getLastBlock() {
+
+    getLasBlock() {
         return this.chain[this.chain.length - 1];
     }
+
     addBlock(block) {
         block.prevHash = this.getLastBlock().hash;
         block.hash = block.getHash();
@@ -15,7 +39,8 @@ class Blockchain {
 
         this.difficulty += Date.now() - parseInt(this.getLastBlock().timestamp) < this.blockTime ? 1 : -1;
     }
-    static isValid() {
+
+    isValid() {
         for (let i = 1; i < this.chain.length; i++) {
             const currentBlock = this.chain[i];
             const prevBlock = this.chain[i-1];
@@ -25,6 +50,9 @@ class Blockchain {
         }
         return true;
     }
+
 }
 
-module.exports = { Blockchain };
+const blockChain = new Blockchain();
+blockChain.addBlock(new Block(Date.now().toString(), { from: "LeeFy", to: "DuyNX", amount: 1000 }));
+console.log(blockChain.chain); 
